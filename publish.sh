0da309cdb987c37eb277865c08953df276ff9a56
Christopher Green <cg433n@email.arizona.edu>
2017-06-01 15:37:41 -0700

    Adds conferences, publish script

--- publish.sh
+++ publish.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
-# Replace "sculpin generate" with "php sculpin.phar generate" if sculpin.phar
-# was downloaded and placed in this directory instead of sculpin having been
-# installed globally.
+if [ $# -ne 1 ]; then
+  echo "usage: ./publish.sh \"commit message\""
+  exit 1;
+fi
 
 sculpin generate --env=prod
-if [ $? -ne 0 ]; then echo "Could not generate the site"; exit 1; fi
 
-# Add --delete right before "output_prod" to have rsync remove files that are
-# deleted locally from the destination too. See README.md for an example.
-rsync -avze 'ssh -p 4668' output_prod/ username@yoursculpinsite:public_html
-if [ $? -ne 0 ]; then echo "Could not publish the site"; exit 1; fi
+git stash
+git checkout master
+
+cp -R output_prod/* .
+rm -rf output_*
+
+git add *
+git commit -m "$1"
+git push origin --all
+
+git checkout develop
+git stash pop
---
+++ source/conferences.md
@@ -0,0 +1,16 @@
+---
+layout: default
+title: Conferences
+---
+
+## Conferences
+
+* [HighEdWeb](https://www.highedweb.org/)
+* [UX Immersion](https://uxi.uie.com/)
+* [DrupalCon](https://events.drupal.org/baltimore2017)
+* [An Event Apart](https://aneventapart.com/)
+* [Nielsen Norman Group](https://www.nngroup.com/ux-conference/)
+* [UA IT Summit](http://itsummit.arizona.edu/)
+* [Squares](http://squaresconference.com/)
+* [Design & Content Conference](https://designcontentconf.com/)
+* [Code4Lib](https://code4lib.org/conference)
